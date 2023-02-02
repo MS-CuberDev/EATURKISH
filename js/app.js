8685d47d8b79828ceea037888a03b13e691185f1
Muhammad <123051716+MS-CuberDev@users.noreply.github.com>
2023-02-02 11:31:31 +0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,6 @@
+function showProd() {
+    const show = document.getElementById("myProd");
+    show.classList.add("prod-block");
+    const showBtn = document.getElementById("myShow");
+    showBtn.classList.add("view-more-none");
+ }
